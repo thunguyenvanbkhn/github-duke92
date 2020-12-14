9cff82084dabbcb1d2b029faf8733bdef48910da
thunguyenvanbkhn <anh.thunguyenbkhn@gmail.com>
2020-12-14 22:23:23 +0700

    commit first project on github

---
+++ springbootMaster/src/main/java/duke/github/springboot/springbootMaster/SpringbootMasterApplication.java
@@ -0,0 +1,13 @@
+package duke.github.springboot.springbootMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMasterApplication.class, args);
+	}
+
+}
---
+++ springbootMaster/src/test/java/duke/github/springboot/springbootMaster/SpringbootMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package duke.github.springboot.springbootMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
